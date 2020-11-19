a0075b24c3687535380a963a89a6e11aca1d0a24
Ayan Dalal <ayan.dalal@olacabs.com>
2020-11-20 01:55:12 +0530

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require('path');
 const express = require('express');
 let cors = require('cors');
 const { start } = require('repl');
-const RequestIp = require('@supercharge/request-ip')
+const RequestIp = require('@supercharge/request-ip');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
