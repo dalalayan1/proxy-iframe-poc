281dc2977b835036212deeae1c33fe953b98ef55
Ayan Dalal <ayan.dalal@olacabs.com>
2020-11-20 02:55:06 +0530

    changes

--- server.js
+++ server.js
@@ -84,7 +84,11 @@ app.get('/request-bin/*', logger, proxy('http://requestbin.net', {
   })
 );
 
-app.use((error, req, res, next) => next());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 app.get(
   "/uidai-proxy/*",
